67616ce4e6cfc5133d05bf3a33850c54436fdafd
G-RIMA <53448883+G-RIMA@users.noreply.github.com>
2021-02-17 11:29:04 +0300

    Add files via upload
    First try at a chatbot that repeats everything a user says

---
+++ Echo_ChatBox.py
@@ -0,0 +1,5 @@
+print("Welcome to My First ChatBot")
+print("This ChatBot is an Echo Bot. It will repeat whatever you say.")
+
+stuff_to_echo = input("Enter something to repeat:")
+print("You said: "+stuff_to_echo)
